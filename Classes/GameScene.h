2611521bcc73ca85324880e7c06a58f9d9a2b3e2
chenjinhui1997 <32393525+chenjinhui1997@users.noreply.github.com>
2017-12-28 21:00:55 +0800

    Add files via upload

--- Classes/GameScene.h
+++ Classes/GameScene.h
@@ -6,8 +6,15 @@
 #include "CannonLayer.h"
 #include "TouchLayer.h"
 #include "Fish.h"
+#include "PanelLayer.h"
+#include "FishingJoyData.h"
 
 USING_NS_CC;
+
+typedef enum{
+	k_Operate_Pause = 0,
+	k_Operate_Resume
+}OperateFlag;
 
 class GameScene :
 	public CCScene
@@ -19,17 +26,21 @@ public:
 	virtual ~GameScene();
 	void cannonAimAt(CCPoint target);
 	void cannonShootTo(CCPoint target);
+
+	void scheduleTimeUp();
+
 protected:
 	BackgroundLayer* _backgroundLayer;
 	FishLayer* _fishLayer;
 	MenuLayer* _menuLayer;
 	CannonLayer* _cannonLayer;
 	TouchLayer* _touchLayer;
-	void preloadResources(void);
+	PanelLayer* _panelLayer;
+	void preloadResources();
 	bool checkOutCollisionBetweenFishesAndBullet(Bullet* bullet);
 	void checkOutCollision();
 	virtual void update(float delta);
 	void fishWillBeCaught(Fish* fish);
+	void alterGold(int delta);
 	void checkOutCollisionBetweenFishesAndFishingNet(Bullet* bulet);
 };
-
